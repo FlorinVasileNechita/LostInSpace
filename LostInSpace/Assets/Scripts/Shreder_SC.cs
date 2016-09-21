8f9aa295cf4438115cdb00c805289fdb821c20ad
FlorinVasileNechita <nechitafv@gmail.com>
2016-09-21 20:59:20 +0300

    lastSyncFromOldOS

--- LostInSpace/Assets/Scripts/Shreder_SC.cs
+++ LostInSpace/Assets/Scripts/Shreder_SC.cs
@@ -5,13 +5,11 @@ public class Shreder_SC : MonoBehaviour {
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        //Gizmos.DrawWireCube(this.transform.position, new Vector3(50,10,0));
         Gizmos.DrawWireCube(this.transform.position, new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z));
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
         Debug.Log("Destorying=" + collider.transform.name);
-        Debug.Log("aaaaaaaaa=");
         Destroy(collider.gameObject);
     }
 }
